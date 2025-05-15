a9d9639f34d85bacff81e893c4a5e6edee047981
Rudra Prasad Nayak <rudraprasadnayak728@gmail.com>
2025-05-15 17:46:57 +0530

    Fixed

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   base: "/",
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   plugins: [react(), tailwindcss()],
 });
